a1557a826a0f8727d33ab6d746be43d8ded6a1a1
Jonathan Jaramillo <125410451+JonathanJara01@users.noreply.github.com>
2023-08-10 14:10:26 -0500

    carrito de compras
    tiene modal, visualización y creación en el back.

--- src/component/ShoppingCar/Cart.jsx
+++ src/component/ShoppingCar/Cart.jsx
@@ -1,30 +1,38 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { resetQuantity } from '../../redux/actions/index';
-import {Link} from 'react-router-dom';
+import { getCarrito, resetQuantity } from '../../redux/actions/index';
+import { useEffect } from 'react';
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cartItems);
   const dispatch = useDispatch();
+  console.log(cartItems)
+  
+  useEffect(() => {
+    dispatch(getCarrito());
+  },[dispatch]); 
 
-  const handleResetQuantity = (productId) => {
-    dispatch(resetQuantity(productId));
+
+  const handleResetQuantity = (cartId) => {
+    dispatch(resetQuantity(cartId));
   };
   
   return (
     <div>
-      <h2>Carrito De Compras</h2>
-      <ul>
+      <h1>Carrito De Compras</h1>
         {cartItems.map((item) => (
-          <li key={item.product.id}>
-            {item.product.name} - Quantity: {item.quantity}
-            <img src={item.product.image} alt={item.product.name} />
-            <button onClick={() => handleResetQuantity(item.product.id)}>Quitar</button>
-          </li>
+        <div key={item.id}>
+          {item.Products.map((product, index) => (
+            <div key={product.id + index}>
+              <h3>Nombre Del Producto: {product.name}</h3>
+              <img src={product.image} alt={product.name} />
+              <p>Cantidad: {product.quantityAvailable}</p>  
+              <p>Precio: {product.price}</p>
+              <h3>Total: {product.price * product.quantityAvailable}</h3>
+            </div>
+          ))}
+          <button onClick={() => handleResetQuantity(item.id)}>Quitar</button>
+          </div>
         ))}
-      </ul>
-      <Link to='/' className='nav-link'>
-        <button>Home</button>
-      </Link>
     </div>
   );
 };
