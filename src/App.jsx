ba221ff420ee5cc959f729f6271c79ad04adcdbc
lucas1visa <visalucas557@gmail.com>
2023-08-10 23:30:00 -0300

    solucion

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import About from './component/About/About';
 import LoadingScreen from './component/Loading/Loading';
 import Contacto from './component/Contacto/Contacto';
 import DashboardAdmin from './component/DashboardAdmin/DashboardAdmin';
+import GraphAdmin from './component/DashboardAdmin/GraphAdmin';
 
 function App() {
   const dispatch = useDispatch();
@@ -34,12 +35,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App-container">
-      <div className="container">
+    <div >
         <div><NavbarComponent /></div>
         {isLoading && <LoadingScreen />} {/* Mostrar LoadingScreen siempre que isLoading sea true */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/grafica" element={<GraphAdmin />} />
           <Route path="/admin" element={<DashboardAdmin/>} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/account/register/" element={<UserProfile />} />
@@ -51,7 +52,6 @@ function App() {
         </Routes>
         <div><Footer /></div>
       </div>
-    </div>
   );
 }
 
