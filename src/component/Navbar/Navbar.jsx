cecc569f87700a3e73f20b81f983764c4334418d
JoseAntSo <josesoria7711@gmail.com>
2023-08-13 18:16:17 -0300

    LOGIN CON AUTENTICACION

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -18,10 +18,7 @@ import Search from "../SearchBar/SearchBar";
 import { useEffect, useState } from "react";
 // importamos todos los componentes de para el formulario de login
 import { FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
-// importamos la funcion de validacion para los inputs
-import validate from "./validate";
-// importamos la funcion para despachar los datos y nos devuelva el token
-import { LoginUser } from "../../services/tokenlogin";
+import Login from "../Login/Login";
 
 const NavbarComponent = () => {
   const [selectedOrder, setSelectedOrder] = useState(""); // Estado para el select de ordenamiento
@@ -55,82 +52,6 @@ const NavbarComponent = () => {
     // dejamos de mostrar el boton de logout
     setShowCartClose(false);
   };
-
-  // ====================================== VENTANA EMERGENTE PARA LOOGIN ============================================
-  // estado para controlar la apertura o cierre de la ventana emergente
-  const [showFormLogin, setShowFormLogin] = useState({
-    open: false
-  });
-  // estado local para controlar la informacion en los inputs 
-  const [valuesInputs, setValuesInputs] = useState({
-    email: "",
-    password: ""
-  });
-  // estado para mostrar el boton de logout
-  const [showLogout,setShowLogout]= useState(false);
-  // estado para mostrar el boton de login
-  const [showLogin, setShowLogin]= useState(true);
-  // estado para almacenar los errores de los inputs
-  const [err, setErr] = useState({});
-  // funcion para captura la informacion y almacenarla en el estado local
-  const handleChangeInput = (event) => {
-    const { name, value } = event.target;
-    setValuesInputs({ ...valuesInputs, [name]: value })
-  };
-  // funcion para cerrar el login
-  const handleLogin = () => {
-    setShowFormLogin({
-      open: !showFormLogin.open
-    })
-  }
-  // funcion para despachar la informacion de los inputs y almacenarlo en la DB
-  const handleSubmitLogin = async(e) => {
-    e.preventDefault();
-    // controlamos y validamos los inputs
-    let errorinput = validate(valuesInputs);
-    // en caso de que exista algun error los almacenamos en el estado local
-    setErr(errorinput);
-    //si no existe ningun error despachamos la info
-    if (Object.keys(errorinput).length === 0) {
-      // despachamos la informacion y obtenemos el valor del token
-      let infotoken = await LoginUser(valuesInputs);
-      console.log(infotoken.newToken);
-      // comprobamos el resultado del token, si el usuario y password fueron validados debera devolver
-      // un token, en caso contrario devolvera un objeto vacio
-      if (Object.getOwnPropertyNames(infotoken.newToken).length){
-        // almacenamos la informacion en localstorage del navegador
-        localStorage.setItem("tokenUser",infotoken.newToken);
-        //dejamos de mostrar el componente login
-        setShowFormLogin({
-          open: false
-        });
-        // limpiamos los inputs
-        setValuesInputs({
-          email: "",
-          password: ""
-        })
-        // dejamos de mostrar el boton de login
-        setShowLogin(false);
-        // mostramos el boton de logout
-        setShowLogout(true);
-        console.log('se envio che');
-      } else {
-        console.log('Hubo error en encontrar el usuario o validar su password');
-      }
-    } else {
-      console.log('hay errores man');
-    }
-  };
-  // funcion para elimanar el token y mostar el boton de login
-  const handleLogout = () =>{
-    // quitamos la informacion almacenada en la localstorage
-    localStorage.removeItem('tokenUser');
-    // mostramos el boton de login
-    setShowLogin(true);
-    // dejamos de mostrar el boton de logout
-    setShowLogout(false);
-  }
-  // =========================================================================================================================
 
   const isHomePage = location.pathname === "/";
 
@@ -240,35 +161,7 @@ const NavbarComponent = () => {
             </div>
           )}
           <button className="btn-admin"><Link className="link-admin" to="/admin">Admin</Link></button>
-      {/*=============================================== REGISTRO DE LOGIN ================================================= */}
-      {showLogout && <button className="btn-salir" onClick={handleLogout}>Salir</button>}
-      {showLogin && <button className="btn-entrar" onClick={handleLogin}>Iniciar</button>}
-      <Modal isOpen={showFormLogin.open}>
-        <ModalHeader>
-          Iniciar Sesion
-        </ModalHeader>
-        <ModalBody>
-          <form onSubmit={handleSubmitLogin}>
-            <FormGroup>
-              <Label>email</Label>
-              <Input type="text" name="email" value={valuesInputs.email} onChange={handleChangeInput} />
-              <p>{err.email}</p>
-            </FormGroup>
-            <FormGroup>
-              <Label>password</Label>
-              <Input type="password" name="password" value={valuesInputs.password} onChange={handleChangeInput} />
-              <p>{err.password}</p>
-            </FormGroup>
-            <Button color="primary" type="submit">Iniciar Sesion</Button>
-          </form>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={handleLogin}>Cerrar</Button>
-          <Link to="/account/register/">Registrate</Link>
-          <Link>Recuperar Password</Link>
-        </ModalFooter>
-      </Modal>
-      {/* ============================================= TERMINACION DE LOGIN ====================================================== */}
+      <Login/>
     </Navbar.Collapse>
   </Container>
 </Navbar>
