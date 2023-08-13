d9b3191aaf6d117a7daa62d0d56983a5a0a5be6e
Jonathan Jaramillo <125410451+JonathanJara01@users.noreply.github.com>
2023-08-12 22:37:25 -0500

    searchBar lista
    se soluciono y tiene el mensaje de búsqueda

--- src/component/SearchBar/SearchBar.jsx
+++ src/component/SearchBar/SearchBar.jsx
@@ -3,39 +3,53 @@ import styles from"./SearchBar.module.css"
 import { searchPrducts,getProducts } from "../../redux/actions";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
-import LoadingScreen from "../Loading/Loading";
+import LoadingScreen from "../Loading/Loading"; 
+import ProductNotFound from "./ProductNotFound";
+
 const Search = () => {
   const dispatch = useDispatch();
   const [productName, setProductName] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  console.log('esto es lo que llega por productName: '+ productName)
+  const [isLoading, setIsLoading] = useState(false); 
+  const [productNotFound, setProductNotFound] = useState(false);
 
   const handleChange = () => {
+     setProductNotFound(false); 
   
-  if (productName === '') {
-      dispatch(getProducts());
-    } 
-    else {
-      setIsLoading(true);
-      
-      
-      // Simular una carga de 1 segundo antes de completar la búsqueda
-      setTimeout(() => {
-        dispatch(searchPrducts(productName))
-          .then(() => {
-            setIsLoading(false);
-          })
-          .catch(error => {
-            console.log(error)
-            console.error('Error fetching search results:', error);
-            setIsLoading(false);
-          });
-          dispatch(getProducts([]));
-         alert('No se encontro el nombre del producto que buscas')
-         setProductName('')
-      }, 600);
+    if (productName === '') {
+      dispatch(getProducts())
+        .then(() => {
+          setProductNotFound(true);
+        });
+      console.log(productName);
+      return; // Salir de la función en este punto si productName está vacío
     }
+  
+    setIsLoading(true);
+  
+    // Simular una carga de 1 segundo antes de completar la búsqueda
+    setTimeout(() => {
+      dispatch(searchPrducts(productName))
+        .then(() => {
+          if(productName == productName){
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            dispatch(setProductNotFound(false));
+          }
+        })
+        .catch(() => {
+          if(productName == productName){
+            setIsLoading(false);
+          }
+          setIsLoading(false);
+          dispatch(setProductNotFound(true));
+        });
+    }, 1000); // Cambié el tiempo a 1000 para que sea un segundo
   };
+  
 
+  
 
   return (
     <div className={styles.containerSearch}>
@@ -49,8 +63,10 @@ const Search = () => {
       <Button onClick={handleChange} className="ml-auto m-2">
         <ion-icon name="search-outline"></ion-icon>
       </Button>
-      {isLoading && <LoadingScreen />}
-    
+       {isLoading && <LoadingScreen />}
+       <div className="div-">
+       {productNotFound && <ProductNotFound/>}
+      </div> 
     </div>
   );
 };
