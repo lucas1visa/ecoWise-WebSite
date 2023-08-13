a224ffe98a38a42dc5ba691533f17dd1875da35a
Carla Yamila Colom <121466242+CarColom@users.noreply.github.com>
2023-08-13 00:27:44 -0300

    estilos detail

--- src/component/ProductDetail/ProductDetail.jsx
+++ src/component/ProductDetail/ProductDetail.jsx
@@ -2,12 +2,10 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, getId, addFav } from "../../redux/actions/index";
 import { useParams } from "react-router-dom";
-
-
 import MPButton from "../MPButton/MPButton";
 import "./ProductDetail.css";
 
-const ProductDetail = ({productId, setShowModal}) => {
+const ProductDetail = ({ productId, setShowModal }) => {
   const { id } = useParams();
   const product = useSelector((state) => state.detail[0]);
   const dispatch = useDispatch();
@@ -20,14 +18,11 @@ const ProductDetail = ({productId, setShowModal}) => {
   const [addToCartText, setAddToCartText] = useState("Agregar al carrito");
 
   useEffect(() => {
-    dispatch(getId(productId || id)); 
+    dispatch(getId(productId || id));
     setTimeout(() => {
       setState({ ...state, loading: false });
     }, 1000);
   }, [dispatch, id, productId]);
-
- 
-
 
   const handleAddToCart = () => {
     dispatch(addToCart(product, quantity));
@@ -40,18 +35,15 @@ const ProductDetail = ({productId, setShowModal}) => {
     }
   };
 
- 
-
   return (
     <div className="custom-container">
       <div className="container-fluid">
         <main className="row">
           <div className="col-md-6">
             {state.loading ? (
-               <div className="loading-container">
-               <div className="loading-spinner"> </div>
-             </div>
-             
+              <div className="loading-container">
+                <div className="loading-spinner"></div>
+              </div>
             ) : (
               product && (
                 <img
@@ -65,48 +57,44 @@ const ProductDetail = ({productId, setShowModal}) => {
           <div className="col-md-6 d-flex align-items-center">
             {state.loading ? (
               <p></p>
-            ) : (
-              product ? (
-                <>
-                  <div className="product-info">
-                    <h2 className="h2-name">{product.name}</h2>
-                  <p>{product.description}</p>
-                  <p>Precio: ${product.price}</p>
-                  <p>Cantidad disponible: {product.quantityAvailable}</p>
-                  <p>Categoría: {product.category}</p>
+            ) : product ? (
+              <div className="product-info">
+                <h2 className="h2-name">{product.name}</h2>
+                <p className="p-description">{product.description}</p>
+                <p className="p-price">Precio: ${product.price}</p>
+                <p className="p-description">
+                  Cantidad disponible: {product.quantityAvailable}
+                </p>
+                <p className="p-description">Categoría: {product.category}</p>
+                <div className="corazon-red-fav">
                   {favorites.find(
                     (favProduct) => favProduct.id === product.id
                   ) ? (
-                    <p className="corazon-red-fav">❤️</p>  
+                    <p>❤️</p>
                   ) : (
+                    <button onClick={handleAddFavorite}>🤍</button>
+                  )}
+                </div>
+                <div className="quantity-select d-flex align-items-center ms-sm-5">
+                  <div className="btn-container">
                     <button
                       className="btn btn-button"
-                      onClick={handleAddFavorite}
+                      onClick={handleAddToCart}
                     >
-                      🤍
+                      {addToCartText}
                     </button>
-                  )}
-                  <div className="quantity-select d-flex align-items-center ms-sm-5">
 
-                    <div className="btn-container">  
-                    <button className="btn-button" onClick={handleAddToCart}>
-                  {addToCartText}
-                </button>
-                      <MPButton
-                        titul={product.name}
-                        precio={product.price}
-                        cantidad={quantity}
-                      />
-                  
-        
-                    </div>
+                    <MPButton
+                      titul={product.name}
+                      precio={product.price}
+                      cantidad={quantity}
+                    />
                   </div>
                 </div>
-              </>
+              </div>
             ) : (
               <p>No se encontró el producto</p>
-            )
-            )} 
+            )}
           </div>
         </main>
       </div>
