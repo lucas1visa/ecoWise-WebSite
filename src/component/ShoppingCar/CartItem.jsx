6fb3dec5f90c94d3168b0b95c7bc283ec39021e0
DAC <d-ardila@outlook.com>
2023-08-13 03:55:28 -0500

    Avance importante Carrito

--- src/component/ShoppingCar/CartItem.jsx
+++ src/component/ShoppingCar/CartItem.jsx
@@ -3,36 +3,47 @@ import { useDispatch } from 'react-redux';
 import { removeFromCart } from '../../redux/actions/index';
 import '../ShoppingCar/Cart.css'
 
-const CartItem = ({ product, cartId }) => {
+const CartItem = ({ product, cartId, cartTotal , onSaveCartTotal }) => {
     const dispatch = useDispatch();
     const [quantity, setQuantity] = useState(product.quantitySelected || 1);
 
-    const handleQuantityChange = (e) => {
-        setQuantity(Number(e.target.value));
+    const handleQuantityChange = e => {
+        const newQuantity = Number(e.target.value);
+        setQuantity(newQuantity);
+
+        // Calcula el nuevo total después de cambiar la cantidad
+        const newTotalPrice = product.price * newQuantity;
+        const safeTotalPrice = isNaN(newTotalPrice) ? 0 : newTotalPrice;
+
+        // Llama a la función para almacenar el nuevo total del carrito
+        onSaveCartTotal(cartId, safeTotalPrice);
     };
 
     const handleDeleteCart = () => {
         dispatch(removeFromCart(cartId, product.id));
     };
+    // const handleSaveCartTotal = () => {
+    //     onSaveCartTotal(cartId, totalPrice); // Llamar a la función prop para almacenar el total
+    // };
 
     const totalPrice = product.price * quantity;
-
+    const safeCartTotal = isNaN(cartTotal) ? 0 : cartTotal;
     return (
         <div className='container-cart'>
-            <div className='subContainer-cart'>
             <h3>Nombre Del Producto: {product.name}</h3>
-            <img src={product.image} alt={product.name} className='image-product-cart'/>
+            <img src={product.image} alt={product.name} className='image-product-cart' />
             <p>
-                <label htmlFor={`quantity-select-${product.id}`} className="texto">
+                <label htmlFor={`quantity-select-${product.quantityAvailable}`} className="texto">
                     Cantidad
                 </label>
                 <select
-                    id={`quantity-select-${product.id}`}
+                    id={`quantity-select-${product.quantityAvailable}`}
                     value={quantity}
                     onChange={handleQuantityChange}
                     className="form-select custom-select"
                 >
-                    {Array.from({ length: product.quantityAvailable }, (_, index) => index + 1).map((count) => (
+                    <option value="1">Seleccione cantidad</option> {/* Opción adicional */}
+                    {Array.from({ length: product.quantityAvailable }, (_, index) => index + 1).map(count => (
                         <option key={count} value={count}>
                             {count}
                         </option>
@@ -41,8 +52,9 @@ const CartItem = ({ product, cartId }) => {
             </p>
             <p>Precio: {product.price}</p>
             <h3>Total: {totalPrice}</h3>
-            <button onClick={handleDeleteCart} className='quitar-product-cart'>❌</button>
-            </div>
+            {console.log(safeCartTotal)} {/* Mostrar el total del carrito aquí */}
+            {/* <button onClick={handleSaveCartTotal}>Guardar Total</button> Agregar el botón para guardar el total */}
+            <button onClick={handleDeleteCart}  className='quitar-product-cart'>❌</button>
         </div>
     );
 };
