8cdbdd7f996b412357f406339a17d4a626361ce9
lucas1visa <visalucas557@gmail.com>
2023-08-15 23:30:16 -0300

    s

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,3 @@
-
 import CardProducts from "../CardProducts/CardProducts";
 import "./Home.css";
 import React, { useEffect } from "react";
@@ -10,23 +9,25 @@ import { postPurcharse } from "../../redux/actions";
 const Home = () => {
   const dispatch = useDispatch()
   const location = useLocation()
-    const queryParams = queryString.parse(location.search); 
-    const { payment_id, payment_type, status} = queryParams;
-    console.log(payment_id)
-    if(payment_id !== null){
-    const x = localStorage.getItem("cart");
-    const {userId,idProduct,quantity} = JSON.parse(x);
-    dispatch(postPurcharse(payment_id, payment_type, status, userId, idProduct, quantity));
+  const queryParams = queryString.parse(location.search); 
+  const { payment_id, payment_type, status } = queryParams;
+
+  useEffect(() => {
+    if (payment_id !== null) {
+      const x = localStorage.getItem("cart");
+      const { userId, idProduct, quantity } = JSON.parse(x);
+      dispatch(postPurcharse(payment_id, payment_type, status, userId, idProduct, quantity));
     }
-  
+  }, [dispatch, payment_id, payment_type, status]);
+
   return (
     <div className="container">
       <div className="text-center m-4 title-container">
-      <img
-            src="https://cdn-icons-png.flaticon.com/512/743/743007.png"
-            alt=""
-            className="title-icon"
-          />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/743/743007.png"
+          alt=""
+          className="title-icon"
+        />
         <h6 className="font-weight-bold"></h6>
       </div>
       <CardProducts />
