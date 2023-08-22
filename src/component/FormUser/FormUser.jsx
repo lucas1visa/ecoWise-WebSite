f7e14a16498b8f4c55aa929b687b9938d72fa6ca
JoseAntSo <josesoria7711@gmail.com>
2023-08-22 10:27:58 -0300

    CAMBIOS FORM
    CAMBIOS

--- src/component/FormUser/FormUser.jsx
+++ src/component/FormUser/FormUser.jsx
@@ -10,6 +10,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import validateinput from "./validate";
+import { BsFillEyeFill,BsFillEyeSlashFill } from "react-icons/bs";
 
 const FormularioPRO = () => {
   // objeto de referencia para setear los inputs y para el caso de limpiarlos
@@ -19,62 +20,82 @@ const FormularioPRO = () => {
     email: "",
     phone: "",
     password: "",
-    confirmpassword:""
+    confirmpassword: ""
   }
   // estado para capturar la informacion de los inputs
-  const [form, setForm] = useState({initialstate});
+  const [form, setForm] = useState({ initialstate });
   // estado para almacenar los errores
   const [errors, setErrors] = useState({
     name: "Debe tener mas de 3 letras",
     surname: "Debe tener mas de 3 letras",
-    email: "",
+    email: "Debe contener @ y un dominio Ej: .com , .es",
     phone: "",
     password: "Debe tener entre (6-16) caracteres, contener una Mayuscula y un numero",
   });
+  // estado para poder mostrar la contraseña
+  const [showPass,setShowPass] = useState(true)
+// estado para poder mostrar la confirmacion de la contraseña
+  const [showConfPass,setShowConfPass] = useState(true)
+  // funcion para capturar los cambios de los inputs
   const changeHandler = (event) => {
-    const property = event.target.name;
-    const value = event.target.value;
-    setForm({ ...form, [property]: value });
-    let err = validateinput(form);
-    setErrors({...errors,})
+    let amanashe = event.target.name;
+    let nashe = event.target.value;
+    setForm({ ...form, [amanashe]: nashe });
+    console.log('email', form.email, 'password', form.password,'confpass',form.confirmpassword);
   };
-  console.log('email',form.email,'password',form.password);
+  // funcion para mostrar constraseña
+  const handleShowPass = (props) => {
+    if(props === "btpass"){
+      setShowPass(!showPass);
+    }
+    if(props === "btconfpass"){
+      setShowConfPass(!showConfPass);
+    }
+  }
+// funcion que valida los inputs y los despacha al back en caso de no tener errores
   const submitHandler = async (event) => {
     event.preventDefault();
 
     const requiredFields = ["name", "surname", "email", "phone", "password"];
     const hasMissingFields = requiredFields.some((field) => form[field] === "");
     if (hasMissingFields) {
-        alert("Por favor, complete todos los campos obligatorios");
-        return;
+      alert("Por favor, complete todos los campos obligatorios");
+      return;
     }
-
-    try {
-        const response = await axios.post("https://ecowise-server01.onrender.com/users", form);
-        if (response.status === 200) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Creado con éxito',
-                showConfirmButton: false,
-                timer: 2000,
-            });
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Hubo un error en la solicitud',
-                showConfirmButton: false,
-                timer: 2000,
-            });
-        }
-    } catch (error) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Revise los datos ingresados',
-            showConfirmButton: false,
-            timer: 2000,
-        });
+    let err = await validateinput(form);
+    console.log(err);
+    setErrors(err);
+    if (Object.keys(errors).length === 0) {
+      console.log('se envio nashe');
+    } else {
+      console.log('hay errores chamo');
     }
-};
+    //     try {
+    //         const response = await axios.post("https://ecowise-server01.onrender.com/users", form);
+    //         if (response.status === 200) {
+    //             Swal.fire({
+    //                 icon: 'success',
+    //                 title: 'Creado con éxito',
+    //                 showConfirmButton: false,
+    //                 timer: 2000,
+    //             });
+    //         } else {
+    //             Swal.fire({
+    //                 icon: 'error',
+    //                 title: 'Hubo un error en la solicitud',
+    //                 showConfirmButton: false,
+    //                 timer: 2000,
+    //             });
+    //         }
+    //     } catch (error) {
+    //         Swal.fire({
+    //             icon: 'error',
+    //             title: 'Revise los datos ingresados',
+    //             showConfirmButton: false,
+    //             timer: 2000,
+    //         });
+    // }
+  };
 
 
 
@@ -90,28 +111,46 @@ const FormularioPRO = () => {
       </Typography>
       <form className=" w-80 max-w-screen-lg sm:w-96 mx-auto" onSubmit={submitHandler}>
         <div className="mb-4 flex flex-col gap-6">
-  <div> 
-    <p className="mr-2">Nombre</p> 
-    <Input type="text" size="lg" value={form.name} onChange={changeHandler} name="name" className=" border-black"/>
-  </div>
-  <div className=""> 
-    <p className="mr-2">Apellido</p>
-    <Input type="text" size="lg" value={form.surname} onChange={changeHandler} name="surname" className=" border-black" />
-  </div>
-  <div className="">
-    <p className="mr-2">Telefono</p>
-    <Input type="text" size="lg" value={form.phone} onChange={changeHandler} name="phone" className=" border-black"/>
-  </div>
-  <div className="">
-    <p className="mr-2">E-mail</p>
-    <Input type="text" size="lg" value={form.email} onChange={changeHandler} name="email" className=" border-black"/>
-  </div>
-  <div className="">
-    <p className="mr-2">Contraseña</p>
-    <Input type="text" size="lg" value={form.password} onChange={changeHandler} name="password" className=" border-black"/>
-  </div>
-</div>
-       
+          <div className="">
+            <p className="mr-2">Nombre</p>
+            <Input Color="red" type="text" size="lg" value={form.name} onChange={changeHandler} name="name" className="bg-white border-black" maxLength="40" />
+            <p>{errors.name}</p>
+          </div>
+          <div className="">
+            <p className="mr-2">Apellido</p>
+            <Input type="text" size="lg" value={form.surname} onChange={changeHandler} name="surname" className=" border-black" maxLength="40" />
+            <p>{errors.surname}</p>
+          </div>
+          <div className="">
+            <p className="mr-2">Telefono</p>
+            <Input type="text" size="lg" value={form.phone} onChange={changeHandler} name="phone" className=" border-black" placeholder="387mañerito" />
+          </div>
+          <div className="">
+            <p className="mr-2">E-mail</p>
+            <Input type="email" size="lg" value={form.email} onChange={changeHandler} name="email" className=" border-black" placeholder="abc123@hotmail.com" required />
+            <p>{errors.email}</p>
+          </div>
+          <div className="">
+            <p className="mr-2"> Contraseña</p>
+            <div className="relative flex">
+              <Input type={showPass? "password" : "text"} size="lg" value={form.password} onChange={changeHandler} name="password" className="border-black pr-10" minLength="3" maxLength="16" required/>
+              <button type="button" className="absolute inset-y-0 right-0 px-3 flex items-center" onClick={()=>handleShowPass("btpass")} >
+                {showPass? <BsFillEyeSlashFill className="text-xl" /> : <BsFillEyeFill className="text-xl"/>}
+              </button>
+            </div>
+            <p>{errors.password}</p>
+          </div>
+          <div className="">
+            <p className="mr-2"> Confirmar Contraseña</p>
+            <div className="relative flex">
+              <Input type={showConfPass? "password" : "text"} size="lg" value={form.confirmpassword} onChange={changeHandler} name="confirmpassword" className="border-black pr-10" minLength="3" maxLength="16" required/>
+              <button type="button" className="absolute inset-y-0 right-0 px-3 flex items-center" onClick={()=>handleShowPass("btconfpass")} >
+                {showConfPass? <BsFillEyeSlashFill className="text-xl" /> : <BsFillEyeFill className="text-xl"/>}
+              </button>
+            </div>
+            <p>{errors.confirm}</p>
+          </div>
+        </div>
         <Button className="mt-6 bg-primary-202 text-black" fullWidth type="submit">
           REGISTRATE
         </Button>
@@ -121,18 +160,11 @@ const FormularioPRO = () => {
             <Login></Login>
           </a>
         </Typography>
-         <Checkbox
+        <Checkbox
           label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
+            <Typography  variant="small" color="gray" className="flex items-center font-normal">
               I agree the
-              <a
-                href="#"
-                className="font-medium transition-colors hover:text-gray-900"
-              >
+              <a href="#" className="font-medium transition-colors hover:text-gray-900">
                 &nbsp;Terms and Conditions
               </a>
             </Typography>
--- src/component/FormUser/validate.js
+++ src/component/FormUser/validate.js
@@ -1,10 +1,8 @@
+import axios from "axios";
 
 
-
-const validateinput = (inputs) =>{
-    let errors= {
-        password:""
-    };
+const validateinput = async(inputs) =>{
+    let errors= {};
     // EXPRESIONES REGULARES PARA CONTROLAR PASSWORD
     const isNonWhiteSpace = /^\S*$/;
     const isContainsUppercase = /^(?=.*[A-Z]).*$/;
@@ -12,24 +10,37 @@ const validateinput = (inputs) =>{
     const isContainsNumber = /^(?=.*[0-9]).*$/;
     // EXPRESION REGULAR PARA EL EMAIL
     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    if(inputs.password.length>5){
+    // preguntamos si ese mail se encuentra disponible para crearse
+    const OneMail = await axios.post("/users/oneMail",{email:inputs.email})
+    .then((response)=>{console.log('email',response.data)})
+    .catch((error)=>{console.error(error);
+    errors.email = 'email se encuentra en uso'})
+    // controlamos los inputs en caso de tener algun error mostramos cual es
+    if(inputs.password){
         if(inputs.password.length<17){
             if(!isNonWhiteSpace.test(inputs.password)){
-                errors.password += 'La contraseña no debe contener espacios en blanco';
+                errors.password = 'No debe contener espacios en blanco';
             }
             if(!isContainsUppercase.test(inputs.password)){
-                errors.password += "La contraseña debe contener al menos una letra mayúscula.";
+                errors.password = "Debe contener al menos una letra mayúscula.";
             }
             if(!isContainsNumber.test(inputs.password)){
-                errors.password += 'La contraseña debe contener al menos un numero'
+                errors.password = 'Debe contener al menos un numero'
             }
         } else {
-            errors.password += "La contraseña debe ser menor o igual a 16 carateres"
+            errors.password = "Debe ser menor o igual a 16 carateres"
         }
+    }else{
+        errors.password += 'Tiene que tener mas de 5 caracteres';
     }
-    if(!emailRegex.test(inputs.email)){
-        errors.email = 'Tiene formato erroneo,Sugerencias: 1)conste de escribir bien el dominio. Ej:(.com),(.es) 2)conste de que tenga el @'
+    if (!inputs.name){
+        errors.name = 'Por favor ingrese un nombre'
+    }
+    if(isContainsNumber.test(inputs.name)){
+        errors.name = 'No debe contener numeros'
+    }
+    if(inputs.password !== inputs.confirmpassword){
+        errors.confirm = 'Las contraseñas no coinciden';
     }
 
     return errors;
