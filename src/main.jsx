aa10edac09ea78e66a7158e48c128d300ddeeccb
lucas1visa <visalucas557@gmail.com>
2023-08-23 02:23:59 -0300

    x

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import { Provider } from "react-redux";
 import './index.css';
 import store from './redux/store/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+      <ThemeProvider>
         <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import withMT from "@material-tailwind/react/utils/withMT";
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
   content: [
   "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",
+  "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
   extend: {
@@ -36,4 +39,4 @@ export default {
   },
   },
   plugins: [],
-  };
+  })
